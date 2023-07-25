2da09ccdf6f269d22d3c3a9459d21c7f923d103a
ZunayedDihan <zunayeddihan@gmail.com>
2023-07-25 21:47:42 +0600

    initial tree

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_X6827.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_X6827-user \
+    lineage_X6827-userdebug \
+    lineage_X6827-eng
