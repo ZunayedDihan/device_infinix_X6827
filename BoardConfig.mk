6d72fa252be9e686852554b1fb0540be75d94622
ZunayedDihan <37245252+ZunayedDihan@users.noreply.github.com>
2023-07-26 12:44:42 +0600

    X6827: add AB_OTA_PARTITIONS

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -8,8 +8,18 @@ DEVICE_PATH := device/Infinix/X6827
 
 # A/B
 AB_OTA_UPDATER := true
-AB_OTA_PARTITIONS +=
 BOARD_USES_RECOVERY_AS_BOOT := true
+AB_OTA_PARTITIONS += \
+    boot \
+    dtbo \
+    odm \
+    product \
+    system \
+    system_ext \
+    vbmeta \
+    vbmeta_system \
+    vendor \
+    vendor_boot
 
 # Architecture
 TARGET_ARCH := arm64
